ec800ab00cf080512d98a9113b56f256159b8a8e
Stijn <stijn@ardusit.nl>
2021-02-15 16:09:31 +0100

    Opdracht 1 en 2

---
+++ opdracht 2/form.php
@@ -0,0 +1,40 @@
+<?php
+$nameErr = $emailErr = "";
+$name = $email = " ";
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_POST['Naam'];
+        $email = $_POST['E-mail'];
+}
+
+
+?>
+<div class="content">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+        <label for="Naam">Naam:</label><br>
+        <input type="text"  id="Naam" name="Naam" value="">
+        <span class="error">* <?php echo $nameErr;?></span> <br>
+        <label for="E-mail">E-mail:</label><br>
+        <input type="text"  id="E-mail" name="E-mail" value="">
+        <span class="error">* <?php echo $nameErr;?></span> <br>
+        <input class="button" type="submit" value="Submit">
+        <p><?php echo $name;?> </p>
+        <p><?php echo $email;?> </p>
+    </form>
+
+</div>
+</body>
+</html>
